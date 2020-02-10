e49d322f396da1b68eb3dea2883459a0e392359f
Nawia <pawelmazur1@gmail.com>
2020-02-10 13:13:54 +0100

    Refactored

--- src/Lib.hs
+++ src/Lib.hs
@@ -10,72 +10,108 @@ import Data.Either (fromRight)
 import Data.List
 import Data.Foldable (foldrM)
 
+readExpr :: Env -> String -> IO String
 readExpr envRef input = do
-    toks <- fromRight [] <$> runParserT (parseExpr envRef) () "guardDemo" input
-    Lib.showList . map showExpr <$> evalAll toks where
-        showExpr x = fst x ++ show (snd x)
-
-showList :: Show a => [a] -> String
-showList = unwords . map show
-
+    toks <- fromRight [] <$> runParserT (parseExpr envRef) () "El" input
+    unwords . map showExpr <$> evalAll toks where
+        showExpr x = show $ fst x ++ " " ++ show (snd x)
+        
 parseExpr :: Env -> ParsecT String () IO [Token]
 parseExpr envRef = parseFunc envRef `sepBy` many1 (oneOf " \t\n")
 
-data Func = Func [Token] [Token] Env
+data Func = Func [([Token], [Token], Env)]
           | Prim String
-          
+
 instance Show Func where
-    show (Func args body _) = "(" ++ Lib.showList args ++ "): " ++ Lib.showList body
+    show (Func func) = unwords $ map showFunc func where
+        showFunc (args, body, _) = "(" ++ show args ++ "): " ++ show body
     show (Prim name) = name
-    
+
+instance Eq Func where
+    (Func func1) == (Func func2) = func1 == func2
+    (Prim func1) == (Prim func2) = func1 == func2
+    _ == _                       = False
+
 parseFunc :: Env -> ParsecT String () IO Token
-parseFunc envRef = many (noneOf " \t\n") >>= liftIO . getFunc envRef
+parseFunc envRef = many1 (noneOf " \t\n") >>= liftIO . matchFunc envRef
 
-type Token = (String, [Func])
+type Token = (String, Func)
 
-type Env = IORef [(String, IORef [IORef Func])]
-    
+data FuncRef = FuncRef [IORef ([Token], [Token], Env)]
+             | PrimRef String
+             
+type TokenRef = (String, IORef FuncRef)
+
+type Env = IORef [TokenRef]
+
+printEnv :: Env -> IO ()
+printEnv env = lsFuncs env >>= putStrLn . unlines . map show
+
 nullEnv :: IO Env
 nullEnv = newIORef []
 
 initEnv :: IO Env
 initEnv = do
     env <- nullEnv
-    bindFuncs env [("^[0-9]+$", [Prim "num"]),
-                   ("^[+*/-]$",   [Prim "numop"])]
-
+    bindFuncs env [("^[0-9]+$", Prim "num"),
+                   ("^[+*/-]$", Prim "numop"),
+                   ("inc", Func [([("a", Prim "num")],
+                                  [("a", Prim "nil"), ("+", Prim "numop"), ("1", Prim "num")],
+                                  env)])]
+                                  
 primitives :: [(String, [Token] -> [Token])]
 primitives = [("num",   num),
-              ("numop", nop),
-              ("nil",   nop)]
+              ("numop", nop)]
               
 num, nop :: [Token] -> [Token]
-num ((arg1, [Prim "num"]) : (op, [Prim "numop"]) : (arg2, [Prim "num"]) : xs) =
-    (show $ fromJust (lookup op numOps) (read arg1) (read arg2), [Prim "num"]) : xs
+num ((arg1, Prim "num") : (op, Prim "numop") : (arg2, Prim "num") : xs) =
+    (show $ fromJust (lookup op numOps) (read arg1) (read arg2), Prim "num") : xs
 num args = args
 nop args = args
 
+numOps :: [(String, Integer -> Integer -> Integer)]
 numOps = [("+", (+)),
           ("-", (-)),
           ("*", (*)),
           ("/", div)]
+          
+nil :: IO Func
+nil = return $ Prim "nil"
 
-nil :: IO [Func]
-nil = return [Prim "nil"]
+isBound :: Env -> String -> IO Bool
+isBound envRef name = isJust <$> findFunc (==) envRef name
 
-getFunc :: Env -> String -> IO Token
-getFunc envRef label = do
-    funcs <- find ((label =~) . fst) <$> readIORef envRef
-    (,) label <$> maybe nil (unpack . snd) funcs where
-        unpack funcs = readIORef funcs >>= mapM readIORef
-    
-bindFuncs :: Env -> [(String, [Func])] -> IO Env
+getFunc, matchFunc :: Env -> String -> IO Token
+(getFunc, matchFunc) = (getFunc' (==), getFunc' (=~)) where
+    getFunc' matchOp envRef name = (,) name <$> (findFunc matchOp envRef name >>= maybe nil (unwrap . snd))
+    unwrap funcRef = readIORef funcRef >>= unwrapFunc
+
+findFunc :: (String -> String -> Bool) -> Env -> String -> IO (Maybe TokenRef)
+findFunc matchOp envRef name = find (matchOp name . fst) <$> readIORef envRef
+
+lsFuncs :: Env -> IO [Token]
+lsFuncs envRef = readIORef envRef >>= mapM unwrapToken
+
+bindFuncs :: Env -> [Token] -> IO Env
 bindFuncs envRef bindings = readIORef envRef >>= extendEnv >>= newIORef where
-    extendEnv env = fmap (++ env) (mapM addBinding bindings)
-    addBinding (name, values) = (,) name <$> (mapM newIORef values >>= newIORef)
+    extendEnv env = fmap (++ env) (mapM wrapToken bindings)
+
+wrapToken :: Token -> IO TokenRef
+wrapToken (name, func) = (,) name <$> (wrapFunc func >>= newIORef)
+
+unwrapToken :: TokenRef -> IO Token
+unwrapToken (name, funcRef) = (,) name <$> (readIORef funcRef >>= unwrapFunc)
+
+wrapFunc :: Func -> IO FuncRef
+wrapFunc (Func func) = FuncRef <$> mapM newIORef func
+wrapFunc (Prim func) = return $ PrimRef func
+
+unwrapFunc :: FuncRef -> IO Func
+unwrapFunc (FuncRef func) = Func <$> mapM readIORef func
+unwrapFunc (PrimRef func) = return $ Prim func
 
 evalAll :: [Token] -> IO [Token]
-evalAll = foldrM (\x y -> eval <$> return (x : y)) []
+evalAll = foldrM (\x y -> eval $ x : y) []
 
-eval :: [Token] -> [Token]
-eval args@((_, [Prim func]) : _) = fromJust (lookup func primitives) args
+eval :: [Token] -> IO [Token]
+eval args@((_, Prim func) : _) = return $ maybe args ($ args) (lookup func primitives)
